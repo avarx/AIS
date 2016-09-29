9108508239ca2e52997087677e0d68a18ed0540e
calesanz <calesanz@gmail.com>
2016-09-30 00:34:16 +0200

    added drive encryption using luks

--- AISBase.sh
+++ AISBase.sh
@@ -1,3 +1,7 @@
+
+
+
+
  #!/bin/bash
 ################################################
 echo "---------------------------------------"
@@ -5,8 +9,27 @@ echo -e "\033[32mFormatting drives...\e[0m"
 echo "---------------------------------------"
 echo -e "\033[33menter device for ROOT (probably sda2)\e[0m"
     read DEV
-    mkfs.ext4 /dev/$DEV
-    mount /dev/$DEV /mnt
+    cryptsetup --cipher aes-xts-plain64 --key-size 512 --hash sha512 --iter-time 4000 luksFormat /dev/$DEV
+    cryptsetup open --type luks /dev/$DEV lvm
+    
+    pvcreate /dev/mapper/lvm
+    vgcreate vol0 /dev/mapper/lvm
+    # Adjust swap size according to your system needs.
+    lvcreate --name lv_swap -L 16GB vol0
+    lvcreate --name lv_root -L 85GB vol0
+    lvcreate --name lv_home -l 100%FREE vol0
+    # format root and home
+    mkfs.ext4 /dev/mapper/vol0-lv_root
+    mkfs.ext4 /dev/mapper/vol0-lv_home
+    # mount root
+    mount /dev/mapper/vol0-lv_root /mnt
+    # Mount the home drive
+    mkdir /mnt/home
+    mount /dev/mapper/vol0-lv_home /mnt/home
+    # create swap
+    mkswap /dev/mapper/vol0-lv_swap
+    swapon /dev/mapper/vol0-lv_swap
+
 echo -e "\033[33menter device for BOOT (probably sda1)\e[0m"
     read DEV
     mkfs.vfat -F32 /dev/$DEV
@@ -15,12 +38,12 @@ echo -e "\033[33menter device for BOOT (probably sda1)\e[0m"
 echo "---------------------------------------"
 echo -e "\033[32mInstalling base system...\e[0m"
 echo "---------------------------------------"
-export LANG=de_CH.UTF-8
+export LANG=en_US.UTF-8
 pacstrap /mnt base base-devel
 echo "---------------------------------------"
 echo -e "\033[32mGenerating fstab...\e[0m"
 echo "---------------------------------------"
-genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U -p /mnt >> /mnt/etc/fstab
 # nano /mnt/etc/fstab
 echo "---------------------------------------"
 echo -e "\033[32mGoing into arch chroot...\e[0m"
--- AISChroot.sh
+++ AISChroot.sh
@@ -1,17 +1,24 @@
-#!/bin/bash
+uthor avarx
+# Contributor calesanz
+# 
+# Helping Guide http://www.brandonkester.com/tech/2014/03/16/full-disk-encryption-in-arch-linux-with-uefi.html
+#
+#
 ################################################
 VCONSOLE="/etc/vconsole.conf"
 HOSTNAME="/etc/hostname"
 ARCHCONF="boot/loader/entries/arch.conf"
 LOADER="/boot/loader/loader.conf"
+MKINITCPIOCONF="/etc/mkinitcpio.conf"
+TEMP="/tmp/mkinit.tmp"
 
 echo "---------------------------------------"
 echo -e "\033[32mGenerating language stuff...\e[0m"
 echo "---------------------------------------"
 locale-gen
-echo LANG=de_CH.UTF-8 > /etc/locale.conf
-export LANG=de_CH.UTF-8
-echo "de_CH-latin1" > $VCONSOLE
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+export LANG=en_US.UTF-8
+echo "KEYMAP=de_CH-latin1" > $VCONSOLE
 ln -s /usr/share/zoneinfo/Europe/Zurich /etc/localtime
 hwclock --systohc --utc
 echo "archy" > $HOSTNAME
@@ -20,15 +27,32 @@ systemctl enable dhcpcd
 echo "---------------------------------------"
 echo -e "\033[32mDoing boot stuff...\e[0m"
 echo "---------------------------------------"
+echo -e "\033[33menter device for ROOT (probably sda2)\e[0m"
+read DEV
 pacman -S dosfstools
+# bootctl
 bootctl --path=/boot install
+
+# archconf
+echo -e "\033[32mBoot configuration...\e[0m"
 touch boot/loader/entries/arch.conf
+UUID=$(blkid /dev/$DEV | awk '{print $2}' | sed 's/"//g') 
 echo "title Arch Linux" >> $ARCHCONF
 echo "linux /vmlinuz-linux" >> $ARCHCONF
 echo "initrd /initramfs-linux.img" >> $ARCHCONF
-echo "options root=/dev/sda2 rw elevator=deadline quiet splash resume=/dev/sda1 nmi_watchdog=0" >> $ARCHCONF
-
+echo "options cryptdevice=$UUID:lvm:allow-discards resume=/dev/mapper/vol0-lv_swap root=/dev/mapper/vol0-lv_root rw quiet splash elevator=deadline" >> $ARCHCONF
 echo "default arch" > $LOADER
+echo "bootctl update..."
+bootctl update
+echo "remove hooks mkinitcpioconf..."
+sed  s/^HOOKS=.*$// $MKINITCPIOCONF > $TEMP && mv $TEMP $MKINITCPIOCONF && rm -f $TEMP
+echo "add hooks"
+echo "HOOKS=\"base udev autodetect modconf block keyboard keymap encrypt lvm2 resume filesystems fsck\"" >> $MKINITCPIOCONF
+
+# regenerate ramdisk
+echo "mkinitcpio..."
+mkinitcpio -p linux
+
 echo "---------------------------------------"
 echo -e "\033[33mNow Check fstab!\e[0m"
 echo -e "\033[33mIf fstab is ok; set a password for root with passwd.\e[0m"
